0f5e54fc9b8c2d40d7fe405826f73c9988a0a62f
buikevin96 <buikevin96@gmail.com>
2018-09-05 23:39:39 -0400

    Data Types: Null

---
+++ 6. PHP/9.datatypesNULL.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Data Type: Null</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        h1{
+            color: purple;
+        }
+    </style>
+    
+  </head>
+      <body>
+        <div class="container-fluid">
+            <h1>NULL:</h1>
+            <div>
+<?php
+$novaluevariable;
+var_dump($novaluevariable);
+echo "<br />";
+$novaluevariable = 5;
+?>
+                
+            </div>
+        </div>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js">
+      </script>
+      <script src="js/bootstrap.min.js">
+      </script>
+      </body>
+</html>
