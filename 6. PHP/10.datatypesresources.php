d445412c76b2d6848236d701309908fec109277f
buikevin96 <buikevin96@gmail.com>
2018-09-06 10:40:04 -0400

    Data type: resources

--- 6. PHP/10.datatypesresources.php
+++ 6. PHP/10.datatypesresources.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Data Type: Null</title>
+    <title>Data Type: Resources</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <style>
         h1{
@@ -15,17 +15,13 @@
   </head>
       <body>
         <div class="container-fluid">
-            <h1>NULL:</h1>
+            <h1>Resources:</h1>
             <div>
 <?php
-$novaluevariable;
-var_dump($novaluevariable);
-$novaluevariable = 5;
+$myFile = fopen("sometext.txt", "r"); // Opens a file or URL, (r)eads it
+var_dump($myFile);
 echo "<br />";
-var_dump($novaluevariable);
-$novaluevariable = null;
-echo "<br />";
-var_dump($novaluevariable);
+echo fread($myFile, filesize("sometext.txt")); // Reads file
 ?>
                 
             </div>
